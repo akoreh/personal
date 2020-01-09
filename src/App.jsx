813b7eeb06c076c1aae87aff3d0465461ad1641d
Andrei Koreh <Andrei.Koreh@endava.com>
2020-01-09 13:57:38 +0200

    add loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { TweenMax } from 'gsap';
 
 function App() {
+  const isDev = true;
+
+  const hideLoadingScreen = () => {
+    TweenMax.set(document.querySelector('#loading'), {delay: isDev ? .2 : 1.5, display: 'none'})
+  };
+
+  useEffect(hideLoadingScreen)
+
   return <h1>Andrei Koreh</h1>
 }
 
