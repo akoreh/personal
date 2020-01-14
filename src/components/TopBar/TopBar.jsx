d8ae1b972c972760661b94def1397ae17b3852e1
Andrei Koreh <Andrei.Koreh@endava.com>
2020-01-14 15:38:54 +0200

    top bar animation

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, createRef } from 'react';
 import moment from 'moment';
+import { TweenMax, Expo } from 'gsap';
 
 import { systemTimeContext } from '../../store/SystemTimeProvider';
 import socialIcons from './socialIcons';
+import { IS_DEV, LOADING_SCREEN_TIME, SCROLL_TO_TOP_DURATION } from '../../constants';
 
 import { C } from '../../util';
 
@@ -11,8 +13,26 @@ import TopBarSocialIcon from './TopBarSocialIcon/TopBarSocialIcon';
 
 const TopBar = () => {
     const systemTime = useContext(systemTimeContext);
+    const topBarRef = createRef();
 
-    return <nav className={cls.topBar}>
+    const animateTopBar = () => {
+        const delay = LOADING_SCREEN_TIME + SCROLL_TO_TOP_DURATION;
+        const target = topBarRef.current;
+        
+        if (IS_DEV) {
+            TweenMax.set(target, {y: 0});
+        } else {
+            setTimeout(() => {
+                TweenMax.to(target, 1, {y: 0, ease: Expo.easeOut});
+            }, delay * 1000);
+        }
+    };
+
+    useEffect(() => {
+       animateTopBar();
+    }, []);
+
+    return <nav className={cls.topBar} ref={topBarRef}>
         <div className={cls.left}>
             <h1 className={C(cls.brand, 'no-select')}>K</h1>
         </div>
