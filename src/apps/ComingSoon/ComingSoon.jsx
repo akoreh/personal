df17f71ad7ae3956b45313d65f9c1d80b4981002
Andrei Koreh <Andrei.Koreh@endava.com>
2020-02-18 16:19:00 +0200

    add coming soon app

---
+++ src/apps/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import TumbleWeed from '../../assets/img/tumbleweed.png';
+
+import cls from './ComingSoon.module.scss';
+
+const AppComingSoon = () => {
+    return <div className={cls.comingSoon}>
+        <div className={cls.bounce} style={{backgroundImage: `url(${TumbleWeed})`}}/>
+        <div className={cls.pebble}/>
+        <div className={cls.pebble}/>
+        <div className={cls.pebble}/>
+        <h1 className={cls.message}>Coming Soon!</h1>
+    </div>
+};
+
+export default AppComingSoon;
--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import ParallaxBackground from '../ParallaxBackground/ParallaxBackground';
 import DesktopIcons from './DesktopIcons';
+import Window from '../Window/Window';
+
+import AppComingSoon from '../../apps/ComingSoon/ComingSoon';
+
+import { LOADING_SCROLL_DURATION } from '../../constants';
 
 import backgrounds from './backgrounds';
 
 import cls from './Desktop.module.scss';
 
-const Desktop = () => (
-    <div className={cls.desktop} style={{paddingTop: '5rem'}}>
+const Desktop = () => {
+    const [comingSoon, setComingSoon] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setComingSoon(true), LOADING_SCROLL_DURATION * 1000);
+    }, []);
+
+    return <div className={cls.desktop} style={{paddingTop: '5rem'}}>
         <ParallaxBackground layers={backgrounds[0].layers}/>
         <DesktopIcons />
-    </div>
-);
+        {comingSoon && <Window style={{width: '40%', height: '45vh', zIndex: 500}}> 
+            <AppComingSoon />        
+        </Window>}
+    </div>;
+}
 
 export default Desktop;
