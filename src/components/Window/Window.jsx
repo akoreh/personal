eff3119bc0844cb267c25c0c8f11ab91a481c7f0
Koreh Andrei <koreh.andrei20@gmail.com>
2020-02-18 18:54:14 +0200

    implement window drag

--- src/components/Window/Window.jsx
+++ src/components/Window/Window.jsx
@@ -1,24 +1,29 @@
-import React, { useState } from 'react';
+import React, { createRef, useState } from 'react';
+import Draggable from 'react-draggable';
 
 import { C } from '../../util';
 
 import cls from './Window.module.scss';
 
-const Window = ({ style, children }) => {
+const Window = ({ id, style, children }) => {
     const [isFullscreen, setIsFullscreen] = useState(false);
+    const handleId = `window__handle-${id}`;
 
     const onToggleFulscreen = () => setIsFullscreen(!isFullscreen);
 
-    return <div className={C(cls.window, isFullscreen ? cls.fullscreen : '')} style={style}>
-        <div className={cls.topBar}>
-            <div className={cls.button} />
-            <div className={cls.button} />
-            <div className={cls.button} onClick={onToggleFulscreen}/>
+    return <Draggable handle={`#${handleId}`} defaultPosition={{x: window.innerWidth / 3.5, y: window.innerHeight / 5}}>
+        <div className={C(cls.window, isFullscreen ? cls.fullscreen : '')} style={style}>
+            <div id={handleId} className={cls.dragHandle}/>
+            <div className={cls.topBar}>
+                <div className={cls.button} />
+                <div className={cls.button} />
+                <div className={cls.button} onClick={onToggleFulscreen}/>
+            </div>
+            <div className={cls.content}>
+                {children}
+            </div>
         </div>
-        <div className={cls.content}>
-            {children}
-        </div>
-    </div>;
+    </Draggable>;
 };
 
 export default Window;
